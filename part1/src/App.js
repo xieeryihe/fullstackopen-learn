575869a036c58c1b0668093b9440e1a390c9a7b0
xieeryihe <wangyingchuan@meituan.com>
2024-07-29 17:02:39 +0800

    Exercise 1.5 pro, 使用解构赋值传参

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,18 @@
-const Header = ({course}) => <h1>{course.name}</h1>
+const Header = ({course : {name} }) => <h1>{name}</h1>
 
-const Content = ({course}) => course.parts.map(part => <Part part={part} key={part.name} />)
+const Content = ({course : {parts} }) => parts.map(part => <Part part={part} key={part.name} />)
 
 const Part = ({part}) => <p>{part.name} {part.exercises}</p>
 
-const Total = ({course}) => <p>Number of exercises {course.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+const Total = ({course : {parts}}) => <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
     ]
   }
 
