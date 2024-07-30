a806482491c916b824e2fd82996c82ad5e55a9ba
xieeryihe <wangyingchuan@meituan.com>
2024-07-30 15:49:30 +0800

    course 1.d 点击操作历史

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,19 +1,34 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+const History = ({operationList}) => {
+  if (operationList.length === 0) return <div>Try to press the button.</div>
+  else return <div>Operation history: {operationList.join(' ')}</div>
+}
 
-  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
-  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1})
+const Button = ({onClick, text}) => {return <button onClick={onClick}>{text}</button>}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [operationList, setList] = useState([])
+
+  const handleLeftClick = () => {
+    setLeft(left + 1)
+    setList(operationList.concat('L'))
+  }
+
+  const handleRightClick = () => {
+    setRight(right + 1)
+    setList(operationList.concat('R'))
+  }
 
   return (
     <div>
-      <span style={{ width: '30px', display: 'inline-block', textAlign: 'center'}}>{clicks.left}</span>
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      <span style={{ width: '30px', display: 'inline-block', textAlign: 'center'}}>{clicks.right}</span>
+      <span style={{ width: '30px', display: 'inline-block', textAlign: 'center'}}>{left}</span>
+      <Button onClick={handleLeftClick} text='left'/>
+      <Button onClick={handleRightClick} text='right'/>
+      <span style={{ width: '30px', display: 'inline-block', textAlign: 'center'}}>{right}</span>
+      <History operationList={operationList}/>
     </div>
   )
 }
