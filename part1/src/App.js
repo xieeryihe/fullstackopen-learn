1e685319a59e7f8bde1c30dc3583aedb6079fe9d
xieeryihe <wangyingchuan@meituan.com>
2024-07-30 11:22:25 +0800

    course 1.c 通过事件状态实现时间计数器

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,15 @@
-const Header = ({course : {name} }) => <h1>{name}</h1>
-
-const Content = ({course : {parts} }) => parts.map(part => <Part part={part} key={part.name} />)
-
-const Part = ({part}) => <p>{part.name} {part.exercises}</p>
-
-const Total = ({course : {parts}}) => <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      { name: 'Fundamentals of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 }
-    ]
-  }
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
 
   return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
+    <div>{counter}</div>
   )
 }
 
