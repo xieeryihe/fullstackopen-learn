3426b83e382888750dc246c4770cd7e1d7a8bae1
xieeryihe <wangyingchuan@meituan.com>
2024-07-31 17:29:47 +0800

    Exercise 1.14 finish

--- part1/src/App.js
+++ part1/src/App.js
@@ -11,25 +11,37 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
 
-  const [selected, setSelected] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [maxIndex, setMaxIndex] = useState(0)  // 最大投票数索引
   const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
 
   const getAnecdote = () => {
     const index = Math.floor(Math.random() * anecdotes.length)
-    setSelected(index)
+    setCurrentIndex(index)
   }
+
+  // 更新最大投票数索引
+  const maxVote = Math.max(...votes)
+  const targetIndex = votes.indexOf(maxVote)
+  if(maxIndex !== targetIndex) {
+    setMaxIndex(targetIndex)
+  }
+
   const vote = () => {
     const newVotes = [...votes]
-    newVotes[selected] += 1
+    newVotes[currentIndex] += 1
     setVotes(newVotes)
   }
 
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
-      <div>{`has ${votes[selected]} votes.`}</div>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[currentIndex]}</div>
+      <div>{`has ${votes[currentIndex]} votes.`}</div>
       <button onClick={vote}>vote</button>
       <button onClick={getAnecdote}>next anecdotes</button>
+      <h1>Anecdote with the most votes</h1>
+      <div>{anecdotes[maxIndex]} </div>
     </div>
   )
 }
