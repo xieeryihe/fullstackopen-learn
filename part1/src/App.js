e8bf494881fe3e03306e586eed265435ad01dc42
xieeryihe <wangyingchuan@meituan.com>
2024-07-31 15:05:37 +0800

    Exercise 1.6 finished

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,34 +1,25 @@
 import { useState } from 'react'
 
-const History = ({operationList}) => {
-  if (operationList.length === 0) return <div>Try to press the button.</div>
-  else return <div>Operation history: {operationList.join(' ')}</div>
-}
-
-const Button = ({onClick, text}) => {return <button onClick={onClick}>{text}</button>}
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [operationList, setList] = useState([])
-
-  const handleLeftClick = () => {
-    setLeft(left + 1)
-    setList(operationList.concat('L'))
-  }
-
-  const handleRightClick = () => {
-    setRight(right + 1)
-    setList(operationList.concat('R'))
-  }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const addGood = () => setGood(good+1)
+  const addNeutral = () => setNeutral(neutral+1)
+  const addBad = () => setBad(bad+1)
 
   return (
     <div>
-      <span style={{ width: '30px', display: 'inline-block', textAlign: 'center'}}>{left}</span>
-      <Button onClick={handleLeftClick} text='left'/>
-      <Button onClick={handleRightClick} text='right'/>
-      <span style={{ width: '30px', display: 'inline-block', textAlign: 'center'}}>{right}</span>
-      <History operationList={operationList}/>
+      <h1>give feedback</h1>
+      <Button onClick={addGood} text={"good"}/>
+      <Button onClick={addNeutral} text={"neutral"}/>
+      <Button onClick={addBad} text={"bad"}/>
+      <h1>statistics</h1>
+      <div>{"good"} {good}</div>
+      <div>{"neutral"} {neutral}</div>
+      <div>{"bad"} {bad}</div>
     </div>
   )
 }
