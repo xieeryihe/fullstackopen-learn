5a871c5597e3ceaa34b66670a6090075651ec950
xieeryihe <wangyingchuan@meituan.com>
2024-08-01 16:44:16 +0800

    Course 2.b Controlled component

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,14 +1,34 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const targetNote = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random < 0.5
+    }
+    setNotes(notes.concat(targetNote))
+    setNewNote('')  // 清空输入栏
+  }
+
+  const handleNoteChange = (event) => setNewNote(event.target.value)
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
+        {notes.map( note => <Note key={note.id} note={note} /> )}
       </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
--- react-learn/src/components/Note.js
+++ react-learn/src/components/Note.js
@@ -1,7 +1,7 @@
 const Note = ({ note }) => {
     return (
-      <li>{note.content}</li>
+        <li>{note.content}</li>
     )
-  }
-  
-  export default Note
+}
+
+export default Note
