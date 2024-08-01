208c54dce2bb4a9cd8a6198ee03cdccca10660b0
xieeryihe <wangyingchuan@meituan.com>
2024-08-01 17:04:08 +0800

    Exercise 2.6 init

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,41 +1,24 @@
 import { useState } from 'react'
-import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
-
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const targetNote = {
-      id: notes.length + 1,
-      content: newNote,
-      important: Math.random < 0.5
-    }
-    setNotes(notes.concat(targetNote))
-    setNewNote('')  // 清空输入栏
-  }
-
-  const handleNoteChange = (event) => setNewNote(event.target.value)
-
-  const noteToShow = showAll ? notes : notes.filter(note => note.important)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}> show {showAll ? 'important' : 'all'} </button>
-      </div>
-      <ul>
-        {noteToShow.map( note => <Note key={note.id} note={note} /> )}
-      </ul>
-
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange}/>
-        <button type="submit">save</button>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
       </form>
+      <h2>Numbers</h2>
+      ...
     </div>
   )
 }
