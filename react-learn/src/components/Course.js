a3fd73574ceaa6253ff4c62a54d8929b870a5c81
xieeryihe <wangyingchuan@meituan.com>
2024-08-01 15:14:24 +0800

    Exercise 2.2 finish

--- react-learn/src/components/Course.js
+++ react-learn/src/components/Course.js
@@ -1,8 +1,14 @@
 const Course = ({course: {name, parts}}) => {
+    let sum = 0;
+    for (let i = 0;i < parts.length; i++){
+        sum += parts[i].exercises
+    }
+
     return (
         <>
             <h1>{name}</h1>
             {parts.map((part, i) => <div key={i}>{part.name} {part.exercises}</div>)}
+            <div><strong>{`total of ${sum} exercises`}</strong></div>
         </>
     )
 }
