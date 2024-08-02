4ca142dd9505af37d3056411ae84fb070509d42c
xieeryihe <wangyingchuan@meituan.com>
2024-08-02 15:43:48 +0800

    Exercise 2.10 finish
    Course 2.c install dependencies
    
    Cource 2.c first request
    
    Cource 2.c add useEffect()

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,70 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newNameFilter, setNewNameFilter] = useState('')
+  const [notes, setNotes] = useState([])
 
-  const filteredPersons = newNameFilter 
-    ? persons.filter(person => person.name.toLowerCase().includes(newNameFilter.toLowerCase()))
-    : persons
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
 
-  const onNameChange = (event) => {
-    const changedName = event.target.value
-    setNewName(changedName)
-  }
-
-  const onNumberChange = (event) => {
-    const changedNumber = event.target.value
-    setNewNumber(changedNumber)
-  }
-
-  const onNameFilterChange = (event) => {
-    const changedName = event.target.value
-    let newFilterPersons = []
-    setNewNameFilter(changedName)
-  }
-
-  const addName = (event) => {
-    event.preventDefault()
-    if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-    } else {
-      const newPerson = { name: newName, number: newNumber }
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
-    }
-  }
+  console.log('render', notes.length, 'notes')
 
   return (
     <div>
-      <h2>Phonebook</h2>
-        <div>
-          filter shown with <input value={newNameFilter} onChange={onNameFilterChange}/>
-        </div>
-      <h2>add a new</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={onNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={onNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Members</h2>
-      {filteredPersons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      <h1>Notes</h1>
+      <ul>{notes.map(note => <Note key={note.id} note={note} />)}</ul>
     </div>
   )
 }
-
 export default App
