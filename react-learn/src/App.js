e81c828f2547e4ebc32474b635f5b877468f6170
xieeryihe <wangyingchuan@meituan.com>
2024-08-26 11:28:24 +0800

    Cource 2.c add useEffect()

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,5 +1,22 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
-const App = ({ notes }) => {
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+
+  console.log('render', notes.length, 'notes')
+
   return (
     <div>
       <h1>Notes</h1>
