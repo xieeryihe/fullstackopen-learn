6ee0e59ae53c661511545c3af19c4340f444689e
xieeryihe <wangyingchuan@meituan.com>
2024-08-27 16:03:02 +0800

    'note' init for course 2.d

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -4,20 +4,45 @@ import Note from './components/Note'
 
 const App = () => {
   const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const targetNote = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5
+    }
+    setNotes(notes.concat(targetNote))
+    setNewNote('')  // 清空输入栏
+  }
+
   useEffect(() => {
-    console.log('effect')
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
-        console.log('promise fulfilled')
         setNotes(response.data)
       })
   }, [])
-  console.log('render', notes.length, 'notes')
+
+  const handleNoteChange = (event) => setNewNote(event.target.value)
+  const noteToShow = showAll ? notes : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
-      <ul>{notes.map(note => <Note key={note.id} note={note} />)}</ul>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}> show {showAll ? 'important' : 'all'} </button>
+      </div>
+      <ul>
+        {noteToShow.map( note => <Note key={note.id} note={note} /> )}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
