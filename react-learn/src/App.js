e9095c87f729b2f3e3933d3c439393b19fdb699c
xieeryihe <wangyingchuan@meituan.com>
2024-08-28 11:00:26 +0800

    Course 2.d add error handling for promise

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -39,9 +39,12 @@ const App = () => {
     const changedNote = { ...note, important: !note.important }
   
     noteService
-      .update(id, changedNote)
-      .then(newNote => {
-        setNotes(notes.map(note => note.id !== id ? note : newNote))
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(`the note '${note.content}' was already deleted from server`)
+        setNotes(notes.filter(n => n.id !== id))
       })
   }
   
