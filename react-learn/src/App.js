53484ff7b4cde49bb3e5cafab5d6d7a00e1a8858
xieeryihe <wangyingchuan@meituan.com>
2024-08-29 14:21:26 +0800

    Cource 2.e add error message style and footer

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -3,10 +3,37 @@ import Note from './components/Note'
 import noteService from './services/noteService'
 import './index.css'
 
+const Notification = ({ message }) => {
+  if (message === null || message === '') {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+    </div>
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const addNote = (event) => {
     event.preventDefault()
@@ -44,7 +71,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(`the note '${note.content}' was already deleted from server`)
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -52,6 +84,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}> show {showAll ? 'important' : 'all'} </button>
       </div>
@@ -68,6 +101,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange}/>&nbsp;
         <button type="submit">save</button>
       </form>
+      <Footer/>
     </div>
   )
 }
