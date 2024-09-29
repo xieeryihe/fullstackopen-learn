26e660a5830db14b133642f7cdec8c6f29976ca2
xieeryihe <wangyingchuan@meituan.com>
2024-09-29 10:46:36 +0800

    Course 3.a update 'post' method

--- web-server/index.js
+++ web-server/index.js
@@ -48,14 +48,31 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.post('/api/notes', (request, response) => {
+const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
     : 0
+  return maxId + 1
+}
 
-  const note = request.body
-  note.id = maxId + 1
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
   notes = notes.concat(note)
+
   response.json(note)
 })
 
