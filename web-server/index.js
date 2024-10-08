7d20631259c9aa7f38abcd9bf6596fba187d6946
xieeryihe <wangyingchuan@meituan.com>
2024-10-08 11:19:56 +0800

    Course 3.b add cors and run

--- web-server/index.js
+++ web-server/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-
 app.use(express.json())
+
+const cors = require('cors')
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -10,7 +12,6 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
-
 app.use(requestLogger)
 
 let notes = [
