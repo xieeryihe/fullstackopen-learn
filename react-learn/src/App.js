b44d4a4db34cbc520bb607b7077b7c9f65a7b316
xieeryihe <wangyingchuan@meituan.com>
2024-10-25 10:49:34 +0800

    Course 3.b add CORS

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,23 +1,107 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/noteService'
+import './index.css'
+
+const Notification = ({ message }) => {
+  if (message === null || message === '') {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const targetNote = {
+      content: newNote,
+      date: new Date(),
+      important: Math.random() < 0.5
+    }
+
+    noteService
+      .create(targetNote)
+      .then(note => {
+        setNotes(notes.concat(note))
+        setNewNote('')  // 清空输入栏
+      })
+  }
+
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }, [])
-  console.log('render', notes.length, 'notes')
+
+  const handleNoteChange = (event) => setNewNote(event.target.value)
+  const noteToShow = showAll ? notes : notes.filter(note => note.important)
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+    debugger
+    noteService
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Error: Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+  
   return (
     <div>
       <h1>Notes</h1>
-      <ul>{notes.map(note => <Note key={note.id} note={note} />)}</ul>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}> show {showAll ? 'important' : 'all'} </button>
+      </div>
+      <ul>
+        {noteToShow.map( note => 
+          <Note 
+            key={note.id} 
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          /> )}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>&nbsp;
+        <button type="submit">save</button>
+      </form>
+      <Footer/>
     </div>
   )
 }
--- web-server/index.js
+++ web-server/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-
 app.use(express.json())
+
+const cors = require('cors')  // 跨域
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
